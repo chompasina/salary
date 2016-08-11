93d9a69222461b659e243d6a8c9355b36db79b64
Tommasina <tommasina1@gmail.com>
2016-08-11 09:07:56 -0600

    revised based on Casey's feedback: -removed redundant equals in view -cleaned up controller current user method

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -2,6 +2,6 @@ class UserJob < ActiveRecord::Base
   belongs_to :job
   belongs_to :user
 
-  validates :user_id, presence: true
-  validates :job_id, presence: true
+  validates :user, presence: true
+  validates :job, presence: true
 end

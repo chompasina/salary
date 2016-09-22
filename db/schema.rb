9929450a829584c475335e35633e3228ca895def
Tommasina <chompasina@users.noreply.github.com>
2016-09-22 10:32:25 -0600

    Adding job functionality (#27)
    * still working on resolving double renderer error in slack#index
    
    * refactors conditional in jobs#create
    
    * working on conditional in slack#create
    
    * Bot now responds to user using user input:
    -adds method to confirm name in Slack Client Service
    -fixes syntax in yml file
    -adds bootstrap styling
    -creates poro for job decision tree
    
    * working on checking user in the database
    
    * refactor slack#index so params are passed to slack service
    
    * still working on adding job title to user
    
    * Works on
    -job info handler methods to confirm job data
    -slack client service to retrieve user job data
    
    * getting closer with logic in slack client service, just need to fix the incrementer
    
    * getting closer all the time
    
    * breaking out user info to a new handler
    
    * adding sessions to slack user session
    
    * trying to fix storing user between inputs in sessions in slack controller
    
    * Adds slack user id as a way to verify user. Conditional tree is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921221616) do
+ActiveRecord::Schema.define(version: 20160922153542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160921221616) do
     t.citext  "last_name"
     t.integer "cohort"
     t.integer "input_counter",   default: 0
+    t.string  "slack_user_id"
   end
 
   add_foreign_key "jobs", "users"

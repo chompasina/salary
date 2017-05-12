dd9680a0c6290c8f6fe498c456361738390a9726
Noah Berman <noah@noahberman.org>
2017-05-12 19:52:50 +0200

    fixes a thing for CI

--- spec/services/slack_client_service_spec.rb
+++ spec/services/slack_client_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Slack client service" do
   xit 'makes a request' do
     visit '/slack/event'
-    
-    expect(page.status_code).to eq(200) 
-  end  
+
+    expect(page.status_code).to eq(200)
+  end
 end
